44d38fa2c69bafe3e1260f883df262fe18505564
stern <1536768420@qq.com>
2019-11-13 21:30:16 +0800

    .

---
+++ src/com/tester/DesignPatternTest/CompositeTest.java
@@ -0,0 +1,18 @@
+package com.tester.DesignPatternTest;
+
+/**
+ * @program: Test-For-HappyFarm
+ * @description: Test of the composite pattern.
+ * @author: stern
+ * @create: 2019/11/13
+ **/
+public class CompositeTest {
+
+    public static void main(String[] args)
+    {
+        System.out.println("—————————————-------------------------------------———— Test[Composite]Pattern —————————————-------------------------------------————");
+        System.out.println("");
+        System.out.println("No composite pattern.");
+        System.out.println("—————————————---------------------------------------------- End ————------—————————-------------------------------------————");
+    }
+}
