696436dd48c62b061232c080e8f89be76da8ed01
stern <1536768420@qq.com>
2019-11-13 11:34:56 +0800

    .

--- src/com/tester/DesignPatternTest/ChainOfResponsibilityTest.java
+++ src/com/tester/DesignPatternTest/ChainOfResponsibilityTest.java
@@ -100,24 +100,58 @@ public class ChainOfResponsibilityTest {
         factoryChain.addFactory(farmProcessingFactory);
         factoryChain.addFactory(pastureProcessingFactory);
 
-        Request request1 = new Request();
-        request1.setRequest("Flour");
-        request1.setNum(20);
-        request1.setRepositoryProxy(RepositoryProxy.Instance());
-        Request request2 = new Request();
-        request2.setRequest("Eggcake");
-        request2.setNum(5);
-        request2.setRepositoryProxy(RepositoryProxy.Instance());
-        Request request3 = new Request();
-        request3.setRequest("ChickenKebabs");
-        request3.setNum(10);
-        request3.setRepositoryProxy(RepositoryProxy.Instance());
-        Response response = new Response();
-        factoryChain.setIndex(0);
-        factoryChain.doProcess(request1, response, factoryChain);
-        factoryChain.setIndex(0);
-        factoryChain.doProcess(request2, response, factoryChain);
-        factoryChain.setIndex(0);
-        factoryChain.doProcess(request3, response, factoryChain);
+        while(true) {
+            System.out.println("Now make request of certain product and its number (\"q\" to quit).");
+            System.out.println("The product name: [Eggcake | ChickenKebabs]");
+            Scanner name = new Scanner(System.in);
+            String requestName = name.next();
+            if (requestName.equals("q")) {
+                break;
+            }
+            if (requestName.equals("Eggcake") || requestName.equals("ChickenKebabs")) {
+                Request request = new Request();
+                request.setRequest(requestName);
+                int amount = 0;
+                while (true) {
+                    Scanner temp = new Scanner(System.in);
+                    System.out.println("The amount:");
+                    if (temp.hasNextInt()) {
+                        amount = temp.nextInt();
+                        if (amount > 0) {
+                            request.setNum(amount);
+                            request.setRepositoryProxy(RepositoryProxy.Instance());
+                            break;
+                        } else {
+                            System.out.println("The amount must more than 0.");
+                        }
+                    }
+                }
+                Response response = new Response();
+                factoryChain.setIndex(0);
+                factoryChain.doProcess(request, response, factoryChain);
+            }
+            else {
+                System.out.println("We cannot make this.");
+            }
+        }
+//        Request request1 = new Request();
+//        request1.setRequest("Flour");
+//        request1.setNum(20);
+//        request1.setRepositoryProxy(RepositoryProxy.Instance());
+//        Request request2 = new Request();
+//        request2.setRequest("Eggcake");
+//        request2.setNum(5);
+//        request2.setRepositoryProxy(RepositoryProxy.Instance());
+//        Request request3 = new Request();
+//        request3.setRequest("ChickenKebabs");
+//        request3.setNum(10);
+//        request3.setRepositoryProxy(RepositoryProxy.Instance());
+//        Response response = new Response();
+//        factoryChain.setIndex(0);
+//        factoryChain.doProcess(request1, response, factoryChain);
+//        factoryChain.setIndex(0);
+//        factoryChain.doProcess(request2, response, factoryChain);
+//        factoryChain.setIndex(0);
+//        factoryChain.doProcess(request3, response, factoryChain);
     }
 }
