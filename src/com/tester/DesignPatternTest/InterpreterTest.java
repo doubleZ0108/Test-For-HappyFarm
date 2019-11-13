74bdd2168070c468e4fe350247450edf6eb9bca8
leaf222 <ahtlye@163.com>
2019-11-13 15:33:10 +0800

    Interpreter Iterator Observer

--- src/com/tester/DesignPatternTest/InterpreterTest.java
+++ src/com/tester/DesignPatternTest/InterpreterTest.java
@@ -13,6 +13,7 @@ public class InterpreterTest
 {
     public static void main(String[] args)
     {
+        System.out.println("\n\n-------------Interpreter Test-------------");
         Expression oneLivestock = Interpreter.getLivestockExpression();
         Expression allLivestock = Interpreter.getAllLivestockExpression();
 
