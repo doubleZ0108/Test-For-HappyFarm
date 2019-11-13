03dd4c9b028bf8bc5465d0f953404b4ac1961d79
double Z <doubleZ0108@163.com>
2019-11-13 21:39:46 +0800

    refactor

--- src/com/tester/DesignPatternTest/BridgeTest.java
+++ src/com/tester/DesignPatternTest/BridgeTest.java
@@ -12,18 +12,33 @@ public class BridgeTest {
 
     public static void main(String[] args) {
 
-        System.out.println("----------  Bridge Test----------");
-        System.out.println("Let the cat to yell 3 times..");
+        System.out.println("—————————————-------------------------------------———— Test[Bridge]Pattern —————————————-------------------------------------————");
+        System.out.println("AnimalYell : setYellTimes(YellTimes yellTimes) :  Set animal yell times.");
+        System.out.println("AnimalYell : yell() :  Execute yell action.");
+        System.out.println("");
+
+
+        System.out.println("=== Create a cat:");
         AnimalYell catYell = new CatYell();
+        System.out.println("=== Create yell three times:");
         YellTimes yellThreeTimes = new YellThreeTimes();
+        System.out.println("=== Set cat yell times:");
         catYell.setYellTimes(yellThreeTimes);
+        System.out.println("===Let the cat to yell 3 times..");
         catYell.yell();
 
-        System.out.println("Let the sheep to yell 2 times..");
+
+        System.out.println("=== Create a sheep:");
         AnimalYell sheepYell = new SheepYell();
+        System.out.println("=== Create yell two times:");
         YellTimes yellTwiceTimes = new YellTwiceTimes();
+        System.out.println("=== Set cat yell times:");
         sheepYell.setYellTimes(yellTwiceTimes);
+        System.out.println("===Let the sheep to yell 2 times..");
         sheepYell.yell();
+
+        System.out.println("—————————————---------------------------------------------- End ————------—————————-------------------------------------————");
+
 
     }
 }
